280966d48ef0e2ccfa28773b9fec80ae7538a120
Jacob Rawung <jacob.rawung@student.unsw.edu.au>
2025-08-08 11:30:22 +1000

    update CI file

--- ci.sh
+++ ci.sh
@@ -1,5 +1,12 @@
 source venv/bin/activate
 
-pytest tests/test_all.py --headless > /dev/null
+python -m pytest tests/test_all.py --headless > /dev/null
 
-exit
+PYTEST_EXIT_CODE=$?
+
+if [ $PYTEST_EXIT_CODE -eq 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
--- tests/test_all.py
+++ tests/test_all.py
@@ -6,7 +6,7 @@ sys.path.insert(0, '/home/jacob/python_dash/quantium-start-repo/src')
 def test_001_header(dash_duo):
     dash_duo.start_server(dash_app)
     dash_duo.wait_for_element("#heading", timeout=10)
-    assert dash_duo.find_element("#heading").text == "Pin Morsel Sales 2018 - 2022"
+    assert dash_duo.find_element("#heading").text == "Pink Morsel Sales 2018 - 2022"
 
 def test_002_visualisation(dash_duo):
     dash_duo.start_server(dash_app)
